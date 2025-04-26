cb61512baa4cbb3a464330e0d5c9f23a09f2eec5
“MaguiMaluff” <maguimalu4@gmail.com>
2025-04-26 16:11:04 -0300

    Implementacion de clase Sistema Prestamos

---
+++ src/main/java/Servicios/SistemaPrestamo.java
@@ -0,0 +1,30 @@
+package Servicios;
+
+import Clases.Estado;
+import Clases.Libro;
+import Clases.Prestamo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SistemaPrestamo {
+    private Catalogo catalogo;
+    private Map<String, Prestamo> prestamos;
+
+    public SistemaPrestamo(Catalogo catalogo) {
+        this.catalogo = catalogo;
+        this.prestamos = new HashMap<>();
+    }
+
+    public Prestamo prestarLibro(String isbn) {
+        Libro libro = catalogo.buscarLibroISBN(isbn);
+        libro.setEstado(Estado.PRESTADO);
+        Prestamo prestamo = new Prestamo(libro);
+        prestamos.put(isbn, prestamo);
+        return prestamo;
+    }
+
+    public Prestamo consultarPrestamo(String isbn) {
+        return prestamos.get(isbn);
+    }
+}
